591edd579ad470d47052cb1f8a9caf82ef79a7f5
Ashu0Singh <ashusingh1417@gmail.com>
2023-06-11 11:14:35 +0530

    Homepage Added

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,15 +1,40 @@
 import "./Home.scss";
 
 import React from "react";
+import Farmer from '../../assets/Farmer.png'
 
 const Home = () => {
 	return (
-		<div className="hero-section">
-			<div className="text-content">
-				<div className="heading"></div>
-			</div>
-		</div>
-	);
+        <div className="hero-section">
+            <div className="content">
+                <div className="text-content">
+                    <h1 className="heading">
+                        Experience the Pureness of Wood Pressed Mustard Oil
+                    </h1>
+                    <p className="desc">
+                        We are committed to provide your family cooking medium
+                        with the utmost purity and honesty to the core - Believe
+                        us. Pure mustard oil offers the ideal balance of Omega-3
+                        and Omega-6 fatty acids, crucial for reducing the risk
+                        of heart disease. It also contains disease-fighting
+                        antioxidants, which the body cannot produce in
+						sufficient quantities through normal metabolism.
+						<br />
+						To maintain good health of your family, it is necessary to
+                        obtain these essential fats and antioxidants from food.
+                        Some experts argue that pure mustard oil surpasses olive
+                        oil due to its ideal fatty acid ratio, which is missing
+                        in olive oil. Furthermore, pure mustard oil has a high
+                        smoke point, making it an excellent choice for frying,
+                        with an added benefit of being more cost-effective.
+                    </p>
+                </div>
+                <div className="image">
+                    <img src={Farmer} alt="Farmer" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
